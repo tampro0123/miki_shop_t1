e412db1ead761cc5aba503f964294cd02818434a
tuan luong <tuanlt203@gmail.com>
2022-07-24 23:05:13 +0700

    add, config tailwind and add img to public

--- src/components/Component1.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Component1() {
-  return <div>Component1</div>;
-}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mon': ['Montserrat'],
+        'plf': ['Playfair Display']
+      },
+      colors: {
+        'bgr': '#f5f5f5',
+        'primary-text': '#272727',
+        '2nd-text': '#272727',
+        '3rd-text': '#B78D71',
+        'price-text': '#92715A',
+        'btn': '#251C17',
+      },
+      spacing: {
+        '8': '8px',
+        '16': '16px',
+        '24': '24px',
+      },
+      borderRadius: {
+        '8': '8px',
+        '16': '16px',
+      },
+    },
+  },
+  plugins: [],
+}
