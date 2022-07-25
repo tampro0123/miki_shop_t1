eca91c976d5b749ce32f4c50f6692c2f57e04249
minhngth2409 <minhngth2409@gmail.com>
2022-07-25 21:48:20 +0700

    fix module model

--- src/models/Products.js
+++ src/models/Products.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const ProductSchema = new Schema(
   {
     name: { type: String, required: true},
--- src/models/RefreshToken.js
+++ src/models/RefreshToken.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const RefreshTokenSchema = new Schema(
   {
     userId: { type: String, required: true },
