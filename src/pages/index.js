b8c41945c20ff4926d19cbb4fad59e126e16afd4
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2022-07-26 22:27:58 +0700

    update layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import Page from 'src/components/Page';
 import Footer from 'src/layouts/Footer';
+import AboutSection from 'src/sections/body/AboutSection.js';
+import BestSeller from 'src/sections/body/BestSeller.js';
+import HeroSection from 'src/sections/body/HeroSection.js';
+import LatestAlbum from 'src/sections/body/LatestAlbum.js';
+import Products from 'src/sections/body/Products.js';
 import Header from '../layouts/header/Header.js';
 
 //always import from src folder, not "./", "../", "../../",...
@@ -7,11 +12,11 @@ export default function Home() {
   return (
     <Page title="Home">
       <Header />
-      <HeroSection/>
-      <AboutSection/>
-      <BestSeller/>
-      <LatestAlbum/>
-      <Products/>
+      <HeroSection />
+      <AboutSection />
+      <BestSeller />
+      <LatestAlbum />
+      <Products />
       <Footer />
     </Page>
   );
