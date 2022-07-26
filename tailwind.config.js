b83891afa0e3315317812f6807f99119a2a1acd3
DucAnhMoi <wag0murad@gmail.com>
2022-07-26 10:23:27 +0700

    update tailwindconfig icon globalstyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'mon': ['Montserrat'],
-        'plf': ['Playfair Display']
+        mon: ['Montserrat'],
+        plf: ['Playfair Display'],
       },
       colors: {
-        'bgr': '#f5f5f5',
+        bgr: '#f5f5f5',
+        'bgr-auth': '#fff9f6',
         'primary-text': '#272727',
         '2nd-text': '#272727',
         '3rd-text': '#B78D71',
         'price-text': '#92715A',
-        'btn': '#251C17',
-      },
-      spacing: {
-        '8': '8px',
-        '16': '16px',
-        '24': '24px',
-      },
-      borderRadius: {
-        '8': '8px',
-        '16': '16px',
+        btn: '#251C17',
+        'border-1': '#6E5544',
+        msgEr: '#d2311b',
       },
     },
   },
   plugins: [],
-}
+};
