2ed581fa09228ebfea0ce4e43ed9c5a0b54b089f
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2022-07-27 15:28:33 +0700

    fix complie section

--- src/pages/register.js
+++ src/pages/register.js
@@ -3,24 +3,29 @@ import { Logo } from 'src/components/icons';
 import imgRegister from 'src/assets/Register/Register.jpg';
 import { RegisterFormSection } from 'src/sections/auth';
 import Image from 'next/image';
+import Header from 'src/layouts/Header';
 
 const register = () => {
   return (
-    <div className="flex justify-center">
-      <div className="flex w-[1136px] h-[852px] overflow-hidden">
-        {/* Img */}
-        <Image height={852} width={646} src={imgRegister} alt="imgRegister" />
-        {/* Content */}
-        <div className="w-[490px] bg-bgr-auth">
-          <div className="flex items-center flex-col mt-14">
-            <Logo />
-            <h1 className="font-plf font-bold text-[40px] leading-[48px]">MIKI JEWELRY</h1>
+    <>
+      {' '}
+      <div className="flex justify-center">
+        <div className="flex w-[1136px] h-[852px] overflow-hidden">
+          {/* Img */}
+          <Image height={852} width={646} src={imgRegister} alt="imgRegister" />
+          {/* Content */}
+          <div className="w-[490px] bg-bgr-auth">
+            <div className="flex items-center flex-col mt-14">
+              <Logo />
+              <h1 className="font-plf font-bold text-[40px] leading-[48px]">MIKI JEWELRY</h1>
+            </div>
+            <h3 className="text-xl leading-7 font-bold ml-10 mt-[72px]">Đăng ký tài khoản</h3>
+            <RegisterFormSection />
           </div>
-          <h3 className="text-xl leading-7 font-bold ml-10 mt-[72px]">Đăng ký tài khoản</h3>
-          <RegisterFormSection />
         </div>
       </div>
-    </div>
+      <Header />
+    </>
   );
 };
 
