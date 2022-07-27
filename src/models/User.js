7ab279e2d6bf793b278b32d789f6af93f291dd40
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2022-07-27 16:43:56 +0700

    add birthday in models

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true },
+    birthday: { type: Date, required: true },
     password: { type: String, required: true, minLength: 8 },
     role: { type: String, default: 'user' },
   },
@@ -15,4 +16,3 @@ const UserSchema = new Schema(
 );
 
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
-
