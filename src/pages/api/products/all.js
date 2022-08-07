1ce6110a83f9357ca45e9eb9035cc9a0d8547d19
minhngth2409 <minhngth2409@gmail.com>
2022-08-07 12:55:04 +0700

    Add get all product file

---
+++ src/pages/api/products/all.js
@@ -0,0 +1,23 @@
+import Products from 'src/models/Products';
+import dbConnect from 'src/utils/dbConnect.js';
+
+dbConnect();
+
+const handler = async (req, res) => {
+    const { method } = req;
+
+    switch (method) {
+        case "GET":
+          try {
+            const users = await Products.find({});
+            return res.status(200).json(users);
+          } catch (err) {
+            return res.status(500).json(err);
+          }
+          break;
+        default:
+          return res.status(500).json("Faild to connect to server");
+      }
+}
+
+export default handler;
