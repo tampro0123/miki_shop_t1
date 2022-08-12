192dbabe5b7cb448b164744b69a0d6a1d1879195
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2022-08-12 18:20:17 +0700

    fix CreateProduct delete methods register

--- src/sections/handleAction/CreateProduct.js
+++ src/sections/handleAction/CreateProduct.js
@@ -1,73 +1,64 @@
-import React from 'react'
+import React from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm, useFieldArray } from 'react-hook-form';
 import * as yup from 'yup';
 import axios from 'axios';
 import { FormProviderBox, TextField, TextArea, SlectOption } from 'src/components/hook-form';
 import Button from 'src/components/Button';
-import { useState } from 'react'
-import convertToBase64 from 'src/sections/handleAction/functionHanle/convertImg.js'
+import { useState } from 'react';
+import convertToBase64 from 'src/sections/handleAction/functionHanle/convertImg.js';
 export default function createProduct() {
   const schema = yup.object().shape({
-    nameProduct: yup
-      .string()
-      .required('Nhập tên sản phẩm'),
-    imageMain: yup
-      .mixed()
-      .required('Vui lòng chọn ảnh chính'),
-    desc: yup
-      .string()
-      .required('Vui lòng nhập mô tả sản phẩm'),
-    dynamicForm: yup.array()
-      .of(yup.object().shape({
-        size: yup.string().required("Vui lòng nhập cỡ"),
-        quantity: yup.string().required("Vui lòng số lượng"),
-        price: yup.string().required("Vui lòng giá")
-      })),
-
-
-
+    nameProduct: yup.string().required('Nhập tên sản phẩm'),
+    imageMain: yup.mixed().required('Vui lòng chọn ảnh chính'),
+    desc: yup.string().required('Vui lòng nhập mô tả sản phẩm'),
+    dynamicForm: yup.array().of(
+      yup.object().shape({
+        size: yup.string().required('Vui lòng nhập cỡ'),
+        quantity: yup.string().required('Vui lòng số lượng'),
+        price: yup.string().required('Vui lòng giá'),
+      })
+    ),
   });
   const methods = useForm({
     resolver: yupResolver(schema),
     reValidateMode: 'onBlur',
     defaultValues: {
       imageMain: null,
-      dynamicForm: []
-    }
+      dynamicForm: [],
+    },
   });
-  const { handleSubmit, reset, register, control } = methods;
+  const { handleSubmit, reset, control } = methods;
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'dynamicForm',
   });
 
   // Handle Submit
-  const [baseMainImg, setBaseImgMain] = useState('')
-  const [baseImgs, setBaseImgs] = useState([])
-  const [click, setClick] = useState(false)
-  const [errAdd, setErrAdd] = useState('')
-  let arr
+  const [baseMainImg, setBaseImgMain] = useState('');
+  const [baseImgs, setBaseImgs] = useState([]);
+  const [click, setClick] = useState(false);
+  const [errAdd, setErrAdd] = useState('');
+  let arr;
   const onChange = (e) => {
-    let convertArr = Array.from(e.target.files)
-    convertArr.forEach(async item => {
-      let base64 = await convertToBase64(item)
-      setBaseImgs((prevState) => [...prevState, base64])
-    })
-  }
+    let convertArr = Array.from(e.target.files);
+    convertArr.forEach(async (item) => {
+      let base64 = await convertToBase64(item);
+      setBaseImgs((prevState) => [...prevState, base64]);
+    });
+  };
   const baseImgMain = (e) => {
-    let convertArr = Array.from(e.target.files)
-    convertArr.forEach(async item => {
-      let base64 = await convertToBase64(item)
-      setBaseImgMain(base64)
-    })
-  }
+    let convertArr = Array.from(e.target.files);
+    convertArr.forEach(async (item) => {
+      let base64 = await convertToBase64(item);
+      setBaseImgMain(base64);
+    });
+  };
   const onSubmit = (data) => {
     if (!click) {
-      return setErrAdd("Vui lòng thêm size")
+      return setErrAdd('Vui lòng thêm size');
     }
     if (data && click) {
-
       const dataAdd = axios({
         method: 'POST',
         url: '/api/products/create',
@@ -83,18 +74,17 @@ export default function createProduct() {
           'Content-Type': 'application/json',
         },
       })
-        .then(response => {
-          console.log(response)
+        .then((response) => {
+          console.log(response);
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
       reset({
-        category: "",
-        desc: "",
+        category: '',
+        desc: '',
         dynamicForm: [],
         imageMain: null,
         image: null,
-        nameProduct: "",
-
+        nameProduct: '',
       });
     }
   };
@@ -105,12 +95,12 @@ export default function createProduct() {
     label: 'mt-6 pl-[8px] mb-[8px]',
     area: 'w-full rounded-lg border-solid border-border-1 border-[1px] p-[10px]',
   };
-  const optionCategory = ["Nhẫn", "Vòng cổ", "Bông tai"]
+  const optionCategory = ['Nhẫn', 'Vòng cổ', 'Bông tai'];
   return (
     <div>
-      <FormProviderBox className='px-10 max-w-[750px] ' methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <FormProviderBox className="px-10 max-w-[750px] " methods={methods} onSubmit={handleSubmit(onSubmit)}>
         <TextField
-          className='flex flex-col'
+          className="flex flex-col"
           label={'Nhập tên sản phẩm: '}
           styleLabel={style.label}
           name="nameProduct"
@@ -123,63 +113,62 @@ export default function createProduct() {
             return (
               <div key={item.id} className="flex gap-[10px] w-full relative">
                 <TextField
-                  className='flex flex-col '
+                  className="flex flex-col "
                   label={'Kích cỡ : '}
                   styleLabel={style.label}
                   // name="size"
                   styleInput={style.lgInput}
                   styleMessage={style.message}
                   placeholder="Size..."
-                  type='text'
+                  type="text"
                   name={`dynamicForm.${index}.size`}
                   isArray
                 />
                 <TextField
-                  className='flex flex-col '
+                  className="flex flex-col "
                   label={'Số lượng : '}
                   styleLabel={style.label}
                   styleInput={style.lgInput}
                   styleMessage={style.message}
                   placeholder="Quantity..."
-                  type='number'
+                  type="number"
                   name={`dynamicForm.${index}.quantity`}
                   isArray
                 />
                 <TextField
-                  className='flex flex-col '
+                  className="flex flex-col "
                   label={'Giá sản phẩm: '}
                   styleLabel={style.label}
                   styleInput={style.lgInput}
                   styleMessage={style.message}
                   placeholder="Price..."
-                  type='number'
+                  type="number"
                   name={`dynamicForm.${index}.price`}
                   isArray
                 />
-                <Button type="button"
+                <Button
+                  type="button"
                   className=" w-[200px] text-base absolute right-[-209px] top-[59px] "
                   primary
-
                   onClick={() => remove(index)}
                   classHover="hover:bg-bgr-auth hover:border-[1px] hover:text-black duration-300 hover:border-black"
                 >
                   Xóa
                 </Button>
               </div>
-            )
+            );
           })}
 
-          <Button type="button"
+          <Button
+            type="button"
             onClick={() => {
-              setClick(true)
-              setErrAdd('')
-              append()
+              setClick(true);
+              setErrAdd('');
+              append();
             }}
             className="mt-[32px] w-[200px] text-base"
             primary
-            name='buttonAddNew'
-            {...register('buttonAddNew')}
-
+            name="buttonAddNew"
             classHover="hover:bg-bgr-auth hover:border-[1px] hover:text-black duration-300 hover:border-black"
           >
             Thêm mới
@@ -211,27 +200,27 @@ export default function createProduct() {
         <TextArea
           className="mb-4 flex flex-col"
           label={'Thông tin sản phẩm: '}
-          name='desc'
+          name="desc"
           styleInput={style.area}
-          placeholder='Thông tin của sản phẩm ... '
+          placeholder="Thông tin của sản phẩm ... "
           styleMessage={style.message}
         />
         <SlectOption
-          className=''
+          className=""
           styleInput={style.area}
           valueOption={optionCategory}
-          name='category'
+          name="category"
           styleMessage={style.message}
         />
         <Button
           className="mt-[32px] w-full text-base"
           primary
-          type='submit'
+          type="submit"
           classHover="hover:bg-bgr-auth hover:border-[1px] hover:text-black duration-300 hover:border-black"
         >
           Tạo mới
         </Button>
       </FormProviderBox>
     </div>
-  )
+  );
 }
