828edb4ac4a66c469628233daed5f0c4b4f33c5d
minhngth2409 <minhngth2409@gmail.com>
2022-08-14 11:07:39 +0700

    add media to Feedback Model

--- src/models/Feedback.js
+++ src/models/Feedback.js
@@ -7,6 +7,7 @@ const FeedbackSchema = new Schema(
     user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     content: { type: String, required: true },
     rate: { type: Number, required: true },
+    media: { type: Object, default: { id: '', src:'' } },
     targetId: { type: Schema.Types.ObjectId, required: true, ref: 'Feedback' },
     isReplied: { type: Boolean, default: false },
   },
