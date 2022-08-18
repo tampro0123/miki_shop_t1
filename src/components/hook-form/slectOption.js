db66c0c4c61623eb65cd797155bc0360cff580f5
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2022-08-18 23:43:44 +0700

    fix slectoption

--- src/components/hook-form/slectOption.js
+++ src/components/hook-form/slectOption.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import { useFormContext } from 'react-hook-form';
 export function SlectOption({ name, label, styleLabel, styleInput,
-    styleMessage, className, passwordErr, userNameErr, valueOption = [], valueUpdate, mailErr, ...passProps }) {
+    styleMessage, className, passwordErr, userNameErr , valueOption = [], mailErr, ...passProps }) {
     const {
         register,
         formState: { errors },
     } = useFormContext();
-    return (
-        <div className={className}>
-            <label className={styleLabel} htmlFor={name}>
-                {label}
-            </label>
-            <select id="cars" className={styleInput} {...register(name)} {...passProps}>
-                {valueUpdate ? <option hidden value={valueUpdate}>{valueUpdate}</option> : undefined}
-                {valueOption.map((item, index) => {
-                    return (
-                        <option key={index} value={item}>{item}</option>
-                    )
-                })}
-            </select>
-            <span className={styleMessage}>{errors[name]?.message || userNameErr || passwordErr || mailErr}</span>
-        </div>
-    )
+  return (
+    <div className={className}>
+        <label className={styleLabel} htmlFor={name}>
+        {label}
+        </label>
+        <select id="cars" className={styleInput} {...register(name)} {...passProps}>
+            {valueOption.map((item,index) =>{
+                return (
+                    <option key ={index}  value={item}>{item}</option>
+                )
+            })}
+        </select>
+        <span className={styleMessage}>{errors[name]?.message || userNameErr || passwordErr || mailErr}</span>
+  </div>
+  )
 }
 
