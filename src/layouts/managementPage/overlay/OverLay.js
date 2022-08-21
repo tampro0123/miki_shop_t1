d6d70b26d474b6f821baec1c80af91f2a6412cd9
TheShyn <huyga612@gmail.com>
2022-08-21 21:38:07 +0700

    add protectRouter to management page

--- src/layouts/managementPage/overlay/OverLay.js
+++ src/layouts/managementPage/overlay/OverLay.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import LeftSide from 'src/layouts/managementPage/leftSide/LeftSide.js'
 import HeaderAdmin from 'src/layouts/managementPage/headerAdmin/HeaderAdmin.js'
-export default function OverLay({children}) {
+import { useEffect, useState } from 'react'
+import { dataUser } from 'src/recoils/dataUser'
+import { useRouter } from 'next/router'
+export default function OverLay({ children }) {
+  const user = useRecoilValue(dataUser)
+  const router = useRouter()
+  const [loading, SetLoading] = useState(true)
+  useEffect(() => {
+    SetLoading(true)
+    router.prefetch('/')
+    if (user.role !== 'admin') {
+      router.replace('/')
+    } else {
+      SetLoading(false)
+    }
+  }, [])
+  if (loading) {
+    return <>Loading...</>;
+  }
   return (
-    <div className ='flex'>
-        <LeftSide/>
-        <div className ='max-w-[1280px] w-full ml-[420px]'>
-          <HeaderAdmin/>
-          {children}
-        </div>
+    <div className='flex'>
+      <LeftSide />
+      <div className='max-w-[1280px] w-full ml-[420px]'>
+        <HeaderAdmin />
+        {children}
+      </div>
     </div>
   )
 }
