bd64d78142836e74533b975f4a9ebec0a48c1bc7
tuan <24a4042615@bav.edu.vn>
2022-08-24 22:53:39 +0700

    a

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,6 @@ import { useEffect, useState, useRef, useTransition } from 'react';
 import HeadlessTippy from '@tippyjs/react/headless';
 import SearchItem from './SearchItem';
 import useDebounce from 'src/hooks/useDebounce';
-import request from 'src/utils/request';
 import { Loading, SearchIcon } from '../Icons';
 import axios from 'axios';
 
@@ -11,7 +10,6 @@ function Search() {
   const [filterText, setFilterText] = useState(''); 
   const [searchResult, setSearchResult] = useState([]);
   const [showResult, setShowResult] = useState(false);
-  const [loading, setLoading] = useState(false);
 
   const debouncedValue = useDebounce(filterText, 500);
   const [isPending, startTransition] = useTransition();
@@ -23,17 +21,13 @@ function Search() {
     }
 
     const fetchApi = async () => {
-      // setLoading(true);
-
       const result = await axios({
         method: 'POST',
         url: 'http://localhost:3000/api/search',
-        data: filterText,
+        data: {filterText},
       })
-      console.log(result.data);
-
-      setSearchResult(result);
-      // setLoading(false);
+      const products = await result.data;
+      setSearchResult(products.product);
     };
 
     fetchApi();
@@ -60,25 +54,16 @@ function Search() {
         visible={showResult && searchResult.length > 0}
         render={(attrs) => (
           <div className="" tabIndex="-1" {...attrs}>
-            <div className={' flex flex-col relative w-[550px] h-[500px] bg-gray-100 border border-t-2 shadow-lg  rounded-sm'}>
-              <div className="flex w-full justify-between items-center py-1 fixed top-0">
-                <h4 className="text-lg inline-block">Sản Phẩm : {searchResult.length}</h4>
-                {/* <Link> */}
-                <a className='mr-4'>Tất cả sản phẩm</a>
-                {/* </Link> */}
+            <div className={' flex flex-col relative w-[550px] bg-gray-100 border border-t-2 shadow-lg  rounded-sm'}>
+              <div className="py-1 fixed top-0">
+                <h4 className="text-lg font-semibold inline-block pl-4">Sản Phẩm : {searchResult.length}</h4>
               </div>
-              {/* {searchResult.map((result) => (
-                <AccountItem key={result.id} data={result} />
-              ))} */}
               <div className="w-full h-[2px] bg-slate-500 mt-7"></div>
               {searchResult.length > 0 && (
-                <div className="mt-3 overflow-y-scroll">
-                  <SearchItem />
-                  <SearchItem />
-                  <SearchItem />
-                  <SearchItem />
-                  <SearchItem />
-                  <SearchItem />
+                <div className={searchResult.length > 5 ? "mt-3 overflow-y-scroll h-[450px]" : "mt-3"}>
+                 {searchResult.map((e) => (
+                  <SearchItem key={e._id} product={e} />
+                 ))}
                 </div>
               )}
             </div>
--- src/components/Search/SearchItem.js
+++ src/components/Search/SearchItem.js
@@ -1,15 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
-import img from 'src/assets/product/banner.jpg';
+import FormatPrice from 'src/utils/formatPrice';
 
-export default function SearchItem() {
+export default function SearchItem({ product }) {
   return (
-    <div className="flex hover:bg-slate-200 h-[80px] mb-3 px-2 cursor-pointer flex-grow-0">
-      <Image src={img} width={80} height={80} objectFit="cover" className="rounded-sm flex-auto" />
-      <div className="ml-2 p-1 flex-1">
-        <p className="text-trumcate2">Nhẫn Kim cương Vàng trắng 14K PNJaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        <p className="text-price-text font-bold text-xl">1.250.000 đ</p>
-      </div>
+    <Link href={`/product/${product.slug}`}>
+        <a>
+        <div className="flex hover:bg-slate-200 h-[80px] mb-3 px-2 cursor-pointer flex-grow-0">
+          <Image
+            src={product.images[0].src}
+            width={80}
+            height={80}
+            objectFit="cover"
+            className="rounded-sm flex-auto"
+          />
+          <div className="ml-2 p-1 flex-1">
+            <p className="text-trumcate2">{product.name}</p>
+            <p className="text-price-text font-bold text-xl">
+              {' '}
+              <FormatPrice price={product?.storage[0]?.price} />{' '}
+            </p>
+          </div>
     </div>
+        </a>
+      </Link>
   );
 }
