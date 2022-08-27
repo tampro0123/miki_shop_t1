13823653e4eb893df696898285d534fbd6183c26
TheShyn <huyga612@gmail.com>
2022-08-27 17:43:26 +0700

    add cartState recoil

--- src/recoils/cartState.js
+++ src/recoils/cartState.js
@@ -1,9 +1,20 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 //if you wanna persist state to local, import recoilPersist like below
 import persistAtom from 'src/utils/recoilPersist';
 
 export const cartState = atom({
-  key: 'authentication',
-  default: {},
+  key: 'cartState',
+  default: [],
   effects_UNSTABLE: [persistAtom], //auto persist and sync with local-storage
 });
+
+export const totalCart = selector({
+  key: 'totalCart',
+  get: ({ get }) => {
+    const cart = get(cartState)
+    return cart.length ? cart.reduce((total, item) => {
+      return total + (item.price * item.quantity)
+    }, 0) : 0
+  }
+})
+
