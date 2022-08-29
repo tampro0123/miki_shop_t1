25e9b01c22224163b1fe76d1bba51c5cd678b068
tuan <24a4042615@bav.edu.vn>
2022-08-29 20:40:59 +0700

    fix product detail

--- src/sections/productDetail/RatingProduct.js
+++ src/sections/productDetail/RatingProduct.js
@@ -1,27 +1,34 @@
-import axios from 'axios';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react'
-import { useRecoilValue } from 'recoil';
+import React, { useEffect, useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import Button from 'src/components/Button';
 import { ImgAndVideo } from 'src/components/Icons';
-import { RatingStar, ratingValue } from 'src/components/Rating'
+import { RatingStar, ratingValue } from 'src/components/Rating';
+import { dataUser } from 'src/recoils/dataUser';
+import axiosAuth from 'src/utils/axios';
 import convertToBase64 from '../handleAction/functionHandle/convertImg';
 
-export default function RatingProduct({ product}) {
-    const router = useRouter();
+export default function RatingProduct({ product }) {
+  const ids = useRecoilValue(dataUser);
+  const router = useRouter();
   // value of rating cmt
   const [lengthText, setLengthText] = useState(0);
-  const [textValue, setTextVaLue] = useState();
-  const [mediaCmt, setMediaCmt] = useState([]);
+  const [textValue, setTextVaLue] = useState('');
+  const [mediaCmt, setMediaCmt] = useState('');
   const [typeCmt, setTypeCmt] = useState('');
-  
-  const rating = useRecoilValue(ratingValue);
+
+  const [rating, setRating] = useRecoilState(ratingValue);
 
   const [err, setErr] = useState({ errSend: '', ratingMessErr: false, cmtMessErr: false });
 
+
+  useEffect(() => {
+    setRating(null);
+    setTextVaLue('')
+  }, [product.slug])
   const handleSubmitCmt = async () => {
     if (rating && textValue) {
-      const res = await axios({
+      const res = axiosAuth({
         method: 'POST',
         url: '/api/feedback',
         headers: {
@@ -35,25 +42,24 @@ export default function RatingProduct({ product}) {
           media: { type: typeCmt, src: mediaCmt },
         },
       })
-      .then(() => {
-        router.reload(undefined,{scroll: false});
-      })
-      .catch((error) => {
-        setErr({...err , errSend: error.response.data.message})
-        setTimeout(() => {
-            setErr({...err , errSend: ''})
-
-        }, 3000);
-      });
+        .then(() => {
+          router.reload(undefined, { scroll: false });
+        })
+        .catch((error) => {
+          setErr({ ...err, errSend: error.response.data.message });
+          setTimeout(() => {
+            setErr({ ...err, errSend: '' });
+          }, 3000);
+        });
     } else if (!rating) {
-        setErr({...err , ratingMessErr: true})
+      setErr({ ...err, ratingMessErr: true });
       setTimeout(() => {
-        setErr({...err , ratingMessErr: false})
+        setErr({ ...err, ratingMessErr: false });
       }, 3000);
     } else {
-        setErr({...err , cmtMessErr: true})
+      setErr({ ...err, cmtMessErr: true });
       setTimeout(() => {
-        setErr({...err , cmtMessErr: false})
+        setErr({ ...err, cmtMessErr: false });
       }, 3000);
     }
   };
@@ -76,38 +82,39 @@ export default function RatingProduct({ product}) {
 
   return (
     <div>
-        <div className="w-full min-h-[350px] bg-white px-[58px] py-[18px] rounded-[10px] shadow-md ">
-            <div className="h-[70px]">
-              <h3>Đánh giá sản phẩm này</h3>
-              <RatingStar />
-              {err.ratingMessErr && <span className="text-red-600">Vui lòng Đánh giá sản phẩm</span>}
-            </div>
-            <div className="flex justify-between mt-4">
-              <span className="font-bold">Bình luận*</span>
-              <span>{`Ký tự còn lại: ${250 - lengthText}`}</span>
-            </div>
-            <div className="h-[95px]">
-              <textarea
-                maxLength="250"
-                className="w-full min-h-[70px] px-3 py-2 rounded-8 border border-l border-black"
-                placeholder="Nhập mô tả tại đây"
-                onChange={handleTextChange}
-              />
-              {err.cmtMessErr && <span className="text-red-600">Vui lòng bình luận sản phẩm</span>}
-              <span className="text-red-500 font-bold">{err.errSend}</span>
-            </div>
-            <div className="flex items-center">
-              <span className="font-bold">Thêm hình ảnh/video sản phẩm nếu có:</span>
-              <input id="image" className="hidden" type="file" onChange={(e) => handleImgCmt(e)}></input>
-              <label htmlFor="image" className="inline-block cursor-pointer">
-                <ImgAndVideo />
-              </label>
-              <span className="font-bold text-xl">( {typeCmt})</span>
-            </div>
-            <Button onClick={handleSubmitCmt} primary className={'float-right hover-btn-primary shadow-md'}>
-              Gửi
-            </Button>
-          </div>
+      <div className="w-full min-h-[350px] bg-white px-[58px] py-[18px] rounded-[10px] shadow-md ">
+        <div className="h-[70px]">
+          <h3>Đánh giá sản phẩm này</h3>
+          <RatingStar />
+          {err.ratingMessErr && <span className="text-red-600">Vui lòng Đánh giá sản phẩm</span>}
+        </div>
+        <div className="flex justify-between mt-4">
+          <span className="font-bold">Bình luận*</span>
+          <span>{`Ký tự còn lại: ${250 - lengthText}`}</span>
+        </div>
+        <div className="h-[95px]">
+          <textarea
+            maxLength="250"
+            className="w-full min-h-[70px] px-3 py-2 rounded-8 border border-l border-black"
+            placeholder="Nhập mô tả tại đây"
+            value={textValue}
+            onChange={handleTextChange}
+          />
+          {err.cmtMessErr && <span className="text-red-600">Vui lòng bình luận sản phẩm</span>}
+          <span className="text-red-500 font-bold">{err.errSend}</span>
+        </div>
+        <div className="flex items-center">
+          <span className="font-bold">Thêm hình ảnh/video sản phẩm nếu có:</span>
+          <input id="image" className="hidden" type="file"  onChange={(e) => handleImgCmt(e)}></input>
+          <label htmlFor="image" className="inline-block cursor-pointer">
+            <ImgAndVideo />
+          </label>
+          <span className="font-bold text-xl">( {typeCmt})</span>
+        </div>
+        <Button onClick={handleSubmitCmt} primary className={'float-right hover-btn-primary shadow-md'}>
+          Gửi
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
