4669693513c134f386cfd66cab3cbd3e8637c346
TheShyn <huyga612@gmail.com>
2022-08-30 16:25:30 +0700

    dasdasd

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import Page from 'src/components/Page';
 import CartUser from 'src/sections/cartInfor/CartUser'
 import { useRecoilValue } from 'recoil'
 import { cartState } from 'src/recoils/cartState'
+import { EmptyCart, Close } from 'src/components/Icons'
 export default function cart() {
     const value = useRecoilValue(cartState)
 
+    const [dataCart, setDataCart] = useState([])
+    useEffect(() => {
+        setDataCart(value)
+    }, [value])
     return (
         <Page title="Cart">
             <div className="app mt-[24px] ">
                 <div className="container mt-0">
                     <p>Breadcum</p>
-                    <div className="mt-[48px]">
-                        <CartUser />
-                    </div>
+                    {dataCart.length ?
+                        <div className="mt-[48px]">
+                            <CartUser />
+                        </div>
+                        :
+                        <div>
+                            <EmptyCart />
+                        </div>
+                    }
                 </div>
             </div>
         </Page>
