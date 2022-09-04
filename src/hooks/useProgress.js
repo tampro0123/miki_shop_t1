41575f4d9ccf66881bb07aa70f6ced9aaa3f9830
DucAnhMoi <100296074+DucAnhMoi@users.noreply.github.com>
2022-09-04 17:21:38 +0700

    add loading page

---
+++ src/hooks/useProgress.js
@@ -0,0 +1,8 @@
+import create from 'zustand';
+
+const useProgressStore = create((set) => ({
+    isAnimating: false,
+    setIsAnimating: (isAnimating) => set(() => ({ isAnimating })),
+}));
+
+export default useProgressStore
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,38 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { RecoilRoot } from 'recoil';
 
+import { useEffect } from 'react';
+import 'tailwindcss/tailwind.css';
+import { useRouter } from 'next/router';
+
+import Progress from 'src/components/progress/Progress.js';
+import useProgressStore from 'src/hooks/useProgress';
+
 function MyApp({ Component, pageProps }) {
+  const setIsAnimating = useProgressStore((state) => state.setIsAnimating);
+  const isAnimating = useProgressStore((state) => state.isAnimating);
+  const router = useRouter();
+  useEffect(() => {
+    const handleStart = () => {
+      setIsAnimating(true);
+    };
+    const handleStop = () => {
+      setIsAnimating(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
   return (
     <RecoilRoot>
+      <Progress isAnimating={isAnimating} />
       <Component {...pageProps} />
       <ToastContainer
         bodyClassName="toast"
