38c00926d0110f78082a095a9356e232e1039829
DucAnhMoi <100296074+DucAnhMoi@users.noreply.github.com>
2022-09-07 11:30:45 +0700

    add loading, redirecting

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,12 @@
 import React from 'react'
+import { Loading2 } from './Icons'
 
 const Loading = () => {
     return (
-        <div className="bg-black text-white">
-            <div className="flex text-center justify-center items-center h-[100vh] ">
-                <div className="absolute w-[200px] h-[200px] rounded-[50%] 
-                before:absolute before:content-[''] before:left-[0] 
-                before:top-[0] before:h-full before:w-full 
-                before:shadow-[0_0_5px_rgba(255,255,255,.3)] before:rounded-[50%] animate-loading"></div>
-                <span className="text-[$737373] leading-[200px] tracking-[1px] animate-text">LOADING...</span>
+        <div className="bg-bgr text-black">
+            <div className="flex flex-col text-center justify-center items-center h-[100vh]">
+                <Loading2 />
+                <div className="mt-[80px] text-[52px] text-3rd-text">Loading . . .</div>
             </div>
         </div>
     )
