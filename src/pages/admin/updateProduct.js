4cbaf0585dbf8e50161caf76532c9ba57273e9c6
TheShyn <huyga612@gmail.com>
2022-09-09 20:46:25 +0700

    update page updateProduct

--- src/pages/admin/updateProduct.js
+++ src/pages/admin/updateProduct.js
@@ -1,8 +1,9 @@
 import { LogoIcon } from 'src/components/icons';
 import UpdateProduct from 'src/sections/handleAction/updateProduct.js';
+import OverLay from 'src/layouts/managementPage/overlay/OverLay';
 export default function updateProduct() {
     return (
-        <div>
+        <OverLay>
             <div className="w-[510px] bg-bgr-auth rounded-r-16 mx-auto">
                 <div className="flex items-center flex-col mt-14 justify-center">
                     <LogoIcon />
@@ -11,6 +12,6 @@ export default function updateProduct() {
                 <h1 className="text-xl leading-7 font-bold ml-10 mt-[72px] ">Cập nhật sản phẩm</h1>
                 <UpdateProduct />
             </div>
-        </div>
+        </OverLay>
     )
 }
