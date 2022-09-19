efcbb2e6aa8faaa0afe3c42b80b73c5b0313bb4c
minhngth2409 <minhngth2409@gmail.com>
2022-09-19 12:39:36 +0700

    add logic get all cart

---
+++ src/pages/api/cart/all.js
@@ -0,0 +1,63 @@
+import withAuth from 'src/middleware/withAuth';
+import withCartOwner from 'src/middleware/withCartOwner';
+import Cart from 'src/models/Cart';
+import dbConnect from 'src/utils/dbConnect.js';
+
+const paymentsHandler = async (req, res) => {
+  await dbConnect();
+  const { method } = req;
+  const { page = 1, limit = 0, status } = req.query;
+
+  switch (method) {
+    case "GET":
+      try {
+        const pageInstance = page - 1;
+        const findInstance = {};
+        var totalItems = await Cart.countDocuments();
+        if (status) {
+          findInstance.status = status;
+          const item = await Cart.aggregate([
+            { $match: { "status": status } },
+            { $sort: { createdAt: 1 } },
+            {
+              $group: {
+                _id: "$status",
+                count: { $sum: 1 },
+              }
+            },
+          ]);
+
+          totalItems = await item[0].count;
+        };
+
+        const totalPages = Math.ceil(totalItems / +limit);
+
+        const carts = await Cart.find(findInstance, {
+          createdAt: 1,
+          name: 1,
+          products: 1,
+          payment: 1,
+          receipt: 1,
+          status: 1,
+        }).sort({ createdAt: -1 })
+          .limit(limit)
+          .skip(limit * pageInstance);
+
+        return res.status(200).json({
+          success: true,
+          carts: carts,
+          perPage: +limit,
+          totalItems,
+          totalPages,
+        });
+      } catch (err) {
+        console.log(err)
+        return res.status(500).json({ success: false, message: err });
+      }
+      break;
+    default:
+      return res.status(500).json("Faild to connect to server");
+  }
+};
+
+export default withAuth(withCartOwner(paymentsHandler));
